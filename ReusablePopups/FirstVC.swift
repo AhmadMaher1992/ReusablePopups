6e1e8afb677a44a4b5a22c94f1963f15f6295913
Ahmad Maher <ahmadeisa1992@gmail.com>
2019-09-26 21:35:44 -0700

    Pass data from popup view controller  to first and second VC using Notification center

--- ReusablePopups/FirstVC.swift
+++ ReusablePopups/FirstVC.swift
@@ -11,14 +11,30 @@ import UIKit
 class FirstVC: UIViewController {
 
     @IBOutlet weak var dateLbl: UILabel!
+    var observer: NSObjectProtocol?
     override func viewDidLoad() {
         super.viewDidLoad()
-         NotificationCenter.default.addObserver(self, selector: #selector(handlePopupClosing), name: .saveDataTime, object: nil)
+//         NotificationCenter.default.addObserver(self, selector: #selector(handlePopupClosing), name: .saveDataTime, object: nil)
     }
-    @objc func handlePopupClosing (notification: Notification){
-        let dateVC = notification.object as! DatePopupVC
-        dateLbl.text = dateVC.formattedDate
+//    @objc func handlePopupClosing (notification: Notification){
+//        let dateVC = notification.object as! DatePopupVC
+//        dateLbl.text = dateVC.formattedDate
+//    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+         observer = NotificationCenter.default.addObserver(forName: .saveDataTime, object: nil, queue: OperationQueue.main) { (notification) in
+            let dateVC = notification.object as! DatePopupVC
+            self.dateLbl.text = dateVC.formattedDate
+        }
     }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        if let observer = observer {
+            NotificationCenter.default.removeObserver(observer)
+        }
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if  segue.identifier == "DatePopupVC" {
             let destination = segue.destination as? DatePopupVC
